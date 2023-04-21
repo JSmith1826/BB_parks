994d1a60f8492566c57f7f2d8b505fb629eb7453
unknown <jbancroftsmith@gmail.com>
2023-04-21 12:03:09 -0400

    dump

--- data/html/script_refactor.js
+++ data/html/script_refactor.js
@@ -1,195 +1,215 @@
 let map;
+let fieldData;
+let polygons = [];
+let homePlateMarkers = [];
+let clickMarker;
 let polyline;
-let distanceDisplay;
 
-function initMap() {
-    console.log("Initializing map...");
-    console.log("initMap function executed");
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 41.9898236, lng: -84.3395166 },
-        zoom: 10,
-        mapTypeId: 'satellite'
+function loadJSON() {
+    const url = 'https://raw.githubusercontent.com/JSmith1826/BB_parks/main/data/360_test/output.json';
+    console.log("loadJSON called");
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error("Error fetching JSON data:", error);
+      });
+  }
+  
+  
+
+  function initMap() {
+    console.log("initMap called");
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 39.8283, lng: -98.5795 },
+      zoom: 4,
     });
-
-    distanceDisplay = document.getElementById('distance-display');
-
-    loadJSONData();
-
-    map.addListener('click', async (event) => {
-        const url = 'https://path.to/your/new_json_file.json';
-        const response = await fetch(url);
-        const baseballFieldData = await response.json();
-
-        baseballFieldData.forEach(field => {
-            createHomePlateMarker(field);
-        });
-
-        const clickedPoint = event.latLng;
-        const closest = findClosestHomePlate(clickedPoint, baseballFieldData);
-        measureDistance(closest.field, clickedPoint, closest.polygon);
+  
+    loadJSON().then((data) => {
+      createPolygons(data);
     });
-}
+  }
 
 
-async function loadJSONData() {
-    const url = 'https://path.to/your/new_json_file.json';
-
-    try {
-        const response = await fetch(url);
-        const baseballFieldData = await response.json();
-
-        baseballFieldData.forEach(field => {
-            createPolygon(field);
-            createHomePlateMarker(field);
-        });
-    } catch (error) {
-        console.error('Error fetching JSON data:', error);
-    }
-}
-
-function createPolygon(field) {
-    console.log("Creating polygon for: ", field.field);
-    const coordinates = field.fop.map(coord => {
-        return new google.maps.LatLng(coord[1], coord[0]);
-    });
-
-    const fillColor = '#90EE90';
-
-    const polygon = new google.maps.Polygon({
-        paths: coordinates,
-        strokeColor: fillColor,
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: fillColor,
-        fillOpacity: 0.35
-    });
-
-    polygon.setMap(map);
-
-    field.name = field.field;
-
-    field.polygonObj = polygon;
-
-    polygon.addListener('click', (event) => {
-        measureDistance(field, event.latLng, polygon);
-    });
-}
-
-function measureDistance(field, clickedPoint, polygon) {
-    const homePlate = field.home_plate;
-
-    console.log("Field name: ", field.field);
-    console.log("Home plate coordinates: ", homePlate);
-    console.log("Clicked point coordinates: ", clickedPoint.toJSON());
-
-    let isInsidePolygon = false;
-    let closestPoint = clickedPoint;
-
-    if (polygon) {
-        isInsidePolygon = google.maps.geometry.poly.containsLocation(clickedPoint, polygon);
-        if (!isInsidePolygon) {
-            const tolerance = 0.0001;
-            let intersectionPoint;
-
-            for (let t = 0; t <= 1; t += 0.001) {
-                const interpolatedPoint = google.maps.geometry.spherical.interpolate(
-                    new google.maps.LatLng(homePlate[1], homePlate[0]),
-                    clickedPoint,
-                    t
-                );
-
-                if (isLocationOnEdge(interpolatedPoint, polygon, tolerance)) {
-                    intersectionPoint = interpolatedPoint;
-                    break;
-                }
-            }
-
-            closestPoint = intersectionPoint || google.maps.geometry.poly.closestLocation(clickedPoint, polygon).point;
-        }
-    } else {
-        isInsidePolygon = false;
-    }
-    
-    if (polygon && !isInsidePolygon) {
-        console.log("Intersection point coordinates: ", closestPoint.toJSON());
-    }
-
-    if (polyline) {
-        polyline.setMap(null);
-    }
-
-    polyline = new google.maps.Polyline({
-        path: [new google.maps.LatLng(homePlate[1], homePlate[0]), closestPoint],
-        geodesic: true,
-        strokeColor: '#0000FF',
-        strokeOpacity: 1.0,
-        strokeWeight: 2
-    });
-
-    polyline.setMap(map);
-
-    const fence_length = google.maps.geometry.spherical.computeDistanceBetween(
-        new google.maps.LatLng(homePlate[1], homePlate[0]),
-        closestPoint
-    );
-
-    const click_length = google.maps.geometry.spherical.computeDistanceBetween(
-        new google.maps.LatLng(homePlate[1], homePlate[0]),
-        clickedPoint
-    );
-
-    const fence_lengthInFeet = fence_length * 3.28084;
-    const click_lengthInFeet = click_length * 3.28084;
-
-    updateDistanceDisplay(fence_lengthInFeet, click_lengthInFeet);
-    updateFieldNameDisplay(field.field);
-}
-
-function updateFieldNameDisplay(fieldName) {
-    console.log('fieldName:', fieldName);
-    const fieldNameDisplay = document.getElementById('field-name-display');
-    fieldNameDisplay.textContent = `Field name: ${fieldName}`;
-}
-
-function createHomePlateMarker(field) {
-    const homePlateLatLng = new google.maps.LatLng(field.home_plate[1], field.home_plate[0]);
-
-    const marker = new google.maps.Marker({
-        position: homePlateLatLng,
+function createPolygons(data) {
+    data.forEach((field) => {
+      let fairTerritoryCoordinates = field.fop.map((coord) => ({
+        lat: coord[1],
+        lng: coord[0],
+      }));
+  
+      let foulTerritoryCoordinates = field.foul.map((coord) => ({
+        lat: coord[1],
+        lng: coord[0],
+      }));
+  
+      let fairTerritory = new google.maps.Polygon({
+        paths: fairTerritoryCoordinates,
+        fillColor: "green",
+        fillOpacity: 0.5,
+        strokeWeight: 1,
+      });
+  
+      let foulTerritory = new google.maps.Polygon({
+        paths: foulTerritoryCoordinates,
+        fillColor: "red",
+        fillOpacity: 0.5,
+        strokeWeight: 1,
+      });
+  
+      fairTerritory.setMap(map);
+      foulTerritory.setMap(map);
+      polygons.push({ fair: fairTerritory, foul: foulTerritory });
+  
+      let homePlateMarker = new google.maps.Marker({
+        position: { lat: field.home_plate[1], lng: field.home_plate[0] },
+        title: field.field,
         map: map,
-        title: 'Home Plate',
+      });
+      homePlateMarkers.push(homePlateMarker);
     });
+  }
+    
+  
+  function onMapClick(event) {
+    console.log("onMapClick called");
+    if (clickMarker) {
+      clickMarker.setMap(null);
+    }
+    if (polyline) {
+      polyline.setMap(null);
+    }
+  
+    clickMarker = new google.maps.Marker({
+      position: event.latLng,
+      map: map,
+    });
+  
+    let closestFieldInfo = findClosestFieldInfo(event.latLng);
+    document.getElementById("field-name").innerHTML = `Field Name: ${closestFieldInfo.fieldName}`;
+  
+    let distance = closestFieldInfo.distanceInFeet;
+    document.getElementById("distance").innerHTML = `Distance: ${distance.toFixed(2)} feet`;
+  
+    let fenceDistance = closestFieldInfo.fenceDistanceInFeet;
+    document.getElementById("fence-distance").innerHTML = `Fence Distance: ${fenceDistance.toFixed(2)} feet`;
+  
+    polyline = new google.maps.Polyline({
+      path: [closestFieldInfo.homePlateLatLng, event.latLng],
+      map: map,
+      strokeColor: "blue",
+      strokeWeight: 2,
+    });
+  }  
+
+  
+    
+function findClosestFieldInfo(latLng) {
+    let closestFieldInfo = {
+    fieldName: "",
+    homePlateLatLng: null,
+    distanceInFeet: Infinity,
+    fenceDistanceInFeet: Infinity,
+    closestPointOnBoundary: null,
+    };
+
+    homePlateMarkers.forEach((marker, index) => {
+    let homePlateLatLng = marker.getPosition();
+    let distance = google.maps.geometry.spherical.computeDistanceBetween(latLng, homePlateLatLng) * 3.28084; // Convert meters to feet
+
+    if (distance < closestFieldInfo.distanceInFeet) {
+        closestFieldInfo.fieldName = marker.getTitle();
+        closestFieldInfo.homePlateLatLng = homePlateLatLng;
+        closestFieldInfo.distanceInFeet = distance;
+
+        let fenceDistance = findFenceDistance(latLng, polygons[index]);
+        closestFieldInfo.fenceDistanceInFeet = fenceDistance.distanceInFeet;
+        closestFieldInfo.closestPointOnBoundary = fenceDistance.closestPoint;
+    }
+    });
+
+    return closestFieldInfo;
 }
-
-function updateDistanceDisplay(fence_lengthInFeet, click_lengthInFeet) {
-    document.getElementById('fence-distance-display').textContent = `Fence distance: ${Math.round(fence_lengthInFeet)} feet`;
-
-    document.getElementById('click-distance-display').textContent = `Click distance: ${Math.round(click_lengthInFeet)} feet`;
-}
-
-function findClosestHomePlate(clickedPoint, baseballFieldData) {
-    let closestField;
-    let minDistance = Infinity;
-    let closestPolygon;
-
-    baseballFieldData.forEach(field => {
-        const homePlateLatLng = new google.maps.LatLng(field.home_plate[1], field.home_plate[0]);
-        const distance = google.maps.geometry.spherical.computeDistanceBetween(clickedPoint, homePlateLatLng);
-
-        if (distance < minDistance) {
-            minDistance = distance;
-            closestField = field;
-
-            if (field.polygon) {
-                closestPolygon = new google.maps.Polygon({
-                    paths: field.polygon.map(coord => new google.maps.LatLng(coord[1], coord[0])),
-                    clickable: false
-                });
-            } else {
-                closestPolygon = null;
+    
+function findFenceDistance(latLng, polygon, closestFieldInfo) {
+    let fenceDistance = Infinity;
+    let closestPoint = null;
+  
+    let fairTerritory = polygon.fair;
+    let foulTerritory = polygon.foul;
+  
+    if (!google.maps.geometry.poly.containsLocation(latLng, fairTerritory)) {
+      console.log("Clicked point is outside the fair territory.");
+      [fairTerritory, foulTerritory].forEach((territory) => {
+        let previousVertex = territory.getPath().getAt(territory.getPath().getLength() - 1);
+        territory.getPath().forEach((vertex) => {
+          let intersection = intersectLine(closestFieldInfo.homePlateLatLng, latLng, previousVertex, vertex);
+          if (intersection) {
+            let distance = google.maps.geometry.spherical.computeDistanceBetween(closestFieldInfo.homePlateLatLng, intersection) * 3.28084; // Convert meters to feet
+            if (distance < fenceDistance) {
+              fenceDistance = distance;
+              closestPoint = intersection;
             }
-        }
-    });
+          }
+          previousVertex = vertex;
+        });
+      });
+    } else {
+      fenceDistance = google.maps.geometry.spherical.computeDistanceBetween(closestFieldInfo.homePlateLatLng, latLng) * 3.28084; // Convert meters to feet
+      closestPoint = latLng;
+    }
+  
+    return {
+      distanceInFeet: fenceDistance,
+      closestPoint: closestPoint,
+    };
+  }
+  
 
-    return { field: closestField, polygon: closestPolygon };
-}
+// Helper function to calculate the cross product
+function crossProduct(p1, p2, p3) {
+    let x1 = p2.lat() - p1.lat();
+    let y1 = p2.lng() - p1.lng();
+    let x2 = p3.lat() - p1.lat();
+    let y2 = p3.lng() - p1.lng();
+  
+    return x1 * y2 - x2 * y1;
+  }
+  
+  // Helper function to check if two line segments intersect
+  function doSegmentsIntersect(p1, p2, q1, q2) {
+    let d1 = crossProduct(q1, p1, p2);
+    let d2 = crossProduct(q2, p1, p2);
+    let d3 = crossProduct(p1, q1, q2);
+    let d4 = crossProduct(p2, q1, q2);
+  
+    return (d1 * d2 < 0) && (d3 * d4 < 0);
+  }
+  
+  // Helper function to find the intersection point of two line segments
+  function findIntersectionPoint(p1, p2, q1, q2) {
+    let A1 = p2.lat() - p1.lat();
+    let B1 = p1.lng() - p2.lng();
+    let C1 = A1 * p1.lat() + B1 * p1.lng();
+    let A2 = q2.lat() - q1.lat();
+    let B2 = q1.lng() - q2.lng();
+    let C2 = A2 * q1.lat() + B2 * q1.lng();
+  
+    let det = A1 * B2 - A2 * B1;
+    if (det === 0) {
+      return null;
+    } else {
+      let lat = (B2 * C1 - B1 * C2) / det;
+      let lng = (A1 * C2 - A2 * C1) / det;
+      return new google.maps.LatLng(lat, lng);
+    }
+  }
+  
+  // Intersection function
+  function intersectLine(p1, p2, q1, q2) {
+    if (doSegmentsIntersect(p1, p2, q1, q2)) {
+      return findIntersectionPoint(p1, p2, q1, q2);
+    } else {
+      return null;
+    }
+  }
+  
